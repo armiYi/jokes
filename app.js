0a3c9a1d2da5cfe94cdd463fc26c8e81eb68d88b
Zhangxin <armi112653@gmail.com>
2024-02-23 17:00:36 +0800

    1

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+    wx.cloud.init({
+      // env 参数说明：
+      //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+      //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+      //   如不填则使用默认环境（第一个创建的环境）
+      env: 'rjh-4g7q6b3p7b3b92e1',
+      traceUser: true,
+    });
   },
   globalData: {
     userInfo: null
